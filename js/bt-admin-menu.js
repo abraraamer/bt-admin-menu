c2f34c90acdda9ff9e2923b50b2d76bb29795e8f
Abrar Aamer <abraraamer1@gmail.com>
2023-10-03 15:40:36 +0500

    mobile events fixes

--- js/bt-admin-menu.js
+++ js/bt-admin-menu.js
@@ -4,12 +4,11 @@ jQuery(function ($) {
     $adminmenu = $('#adminmenu');
     $body = $('body');
     $scrollbarwidth = 10;//17 for the default scrollbar
-    mobile_width = 781;
+    mobile_width_max = 781;
     autofold_width = 960;
     admin_menu_editor_always_open_element = '.ws-ame-has-always-open-submenu';
-    
-    $adminmenu.data('wp-responsive', true);//disable wordpress sticky system
-    
+    isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
+
     //align the sub menu on scroll
     $adminmenu.on('scroll', function () {
         if ($adminmenu.find('li.opensub').length) {
@@ -20,12 +19,24 @@ jQuery(function ($) {
             });
         }
     });
-    
+
+    if (('ontouchstart' in window || /IEMobile\/[1-9]/.test(navigator.userAgent)) && $window.outerWidth() > mobile_width_max && $body.hasClass('mobile')) { // Touch screen device.
+        // iOS Safari works with touchstart, the rest work with click.
+        var mobileEvent = isIOS ? 'touchstart' : 'click';
+        $adminmenu.find('>li>a.wp-has-submenu').on(mobileEvent, function (e) {
+            return false;
+        });
+
+        $adminmenu.on(mobileEvent + '.wp-submenu-head', '.wp-submenu-head', function (e) {
+            window.location = $(e.target).parent().siblings('a').attr('href');
+
+        });
+    }
     // whenever we hover over a menu item that has a submenu and not an always open menu
 
     $adminmenu.find('>li').not(admin_menu_editor_always_open_element).on('mouseover', function () {
 
-        if ($window.outerWidth() > mobile_width) {
+        if ($window.outerWidth() > mobile_width_max) {
 
             $adminmenu.find('li.opensub').removeClass('opensub');
 
@@ -50,7 +61,7 @@ jQuery(function ($) {
     });
 
     $adminmenu.find('>li').on('mouseout', function () {
-        if ($window.outerWidth() > mobile_width) {
+        if ($window.outerWidth() > mobile_width_max) {
             closeSubMenus();
             $adminmenu.find('>li').removeClass('opensub');
         }
@@ -62,7 +73,7 @@ jQuery(function ($) {
             $adminmenu.find('>li').not(admin_menu_editor_always_open_element).find(' > .wp-submenu').css({top: '-1000em', left: 'auto'});
         } else {
             if ($window.outerWidth() <= autofold_width && $body.hasClass('auto-fold')) {
-                $adminmenu.find('>li').not(admin_menu_editor_always_open_element+' , .wp-current-submenu').find(' > .wp-submenu').css({top: '-1000em', left: 'auto'});
+                $adminmenu.find('>li').not(admin_menu_editor_always_open_element + ' , .wp-current-submenu').find(' > .wp-submenu').css({top: '-1000em', left: 'auto'});
 
             } else {
                 $adminmenu.find('>li.wp-not-current-submenu').not(admin_menu_editor_always_open_element).find(' > .wp-submenu').css({top: '-1000em', left: 'auto'});
